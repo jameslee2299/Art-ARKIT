4533bd01063c4e52e2928cd2e6397f7e2d0fac93
James Lee <jlee@cyence.net>
2017-11-28 21:57:06 -0800

    Initialized controller with installed ARKit framework. Configured initial starting location to map within space

---
+++ plane shooter game/plane shooter game/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  plane shooter game
+//
+//  Created by James Lee on 11/28/17.
+//  Copyright © 2017 James Lee. All rights reserved.
+//
+
+import UIKit
+import ARKit
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
