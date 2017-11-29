1208948156db18e563df25819715ec19a4112f08
James Lee <jameslee2299@gmail.com>
2017-11-29 14:12:24 -0800

    Updated some comments to explain logic within controller

--- plane shooter game/plane shooter game/ViewController.swift
+++ plane shooter game/plane shooter game/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
     let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
-        self.sceneView.session.run(configuration)
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]  //used for debugging
+        self.sceneView.session.run(configuration)   //initializes configuration environment
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -29,16 +29,15 @@ class ViewController: UIViewController {
     }
 
     func addPlaneNode() {
-        //let planeScene = SCNScene(named: ")
-        let planeScene = SCNScene(named: "art.scnassets/Plane.scn")
-        let planeNode = planeScene?.rootNode.childNode(withName: "plane", recursively: false)
-        planeNode?.position = SCNVector3(0,0,0.1)
-        planeNode?.scale = SCNVector3(0.01, 0.01, 0.01)
+        let planeScene = SCNScene(named: "art.scnassets/Plane.scn")     //this initializes the plane image
+        let planeNode = planeScene?.rootNode.childNode(withName: "plane", recursively: false)       //initializes the node ('withName: "plane"' represents the name of the scenegraph located within the Plane.scn file it titles the object (DOES NOT REPRESENT THE FILE NAME)
+        planeNode?.position = SCNVector3(0,0,0.1)       //0, 0, 0 represents the xyz graph origin
+        planeNode?.scale = SCNVector3(0.01, 0.01, 0.01)     //down scales the plane by 100
         self.sceneView.scene.rootNode.addChildNode(planeNode!)
     }
 
-    @IBAction func addPlane(_ sender: Any) {
-        addPlaneNode()
+    @IBAction func addPlane(_ sender: Any) {        //this is the button caller for 'planez' if the button is clicked this gets called
+        self.addPlaneNode()
     }
     
     
