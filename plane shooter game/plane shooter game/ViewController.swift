d15a295d26d7c8709bd9fa365598f7dc734a798e
James Lee <jameslee2299@gmail.com>
2017-11-29 21:32:30 -0800

    Created a random number generator for the aliens. Also created some UI for buttons that’d represent the planes rotation as well as the go button. They’re connected to boolean statements that do anything yet

--- plane shooter game/plane shooter game/ViewController.swift
+++ plane shooter game/plane shooter game/ViewController.swift
@@ -11,6 +11,12 @@ import ARKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
+    //var timer: Timer!
+    var forward = false
+    var up = false
+    var left = false
+    var right = false
+    var down = false
     let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +37,7 @@ class ViewController: UIViewController {
     func addPlaneNode() {
         let planeScene = SCNScene(named: "art.scnassets/Plane.scn")     //this initializes the plane image
         let planeNode = planeScene?.rootNode.childNode(withName: "plane", recursively: false)       //initializes the node ('withName: "plane"' represents the name of the scenegraph located within the Plane.scn file it titles the object (DOES NOT REPRESENT THE FILE NAME)
-        planeNode?.position = SCNVector3(0,0,0.1)       //0, 0, 0 represents the xyz graph origin
+        planeNode?.position = SCNVector3(0, 0.3, 0)       //0, 0, 0 represents the xyz graph origin
         planeNode?.scale = SCNVector3(0.01, 0.01, 0.01)     //down scales the plane by 100
         self.sceneView.scene.rootNode.addChildNode(planeNode!)
     }
@@ -39,14 +45,77 @@ class ViewController: UIViewController {
     func addAlienNode() {
         let alienScene = SCNScene(named: "art.scnassets/alien.scn")
         let alienNode = alienScene?.rootNode.childNode(withName: "alien", recursively: false)
-        alienNode?.position = SCNVector3(0.3, 0, 0.3)
+        alienNode?.position = SCNVector3(self.randomLocationGenerator(firstNum: -4,secondNum: 4),
+                                         self.randomLocationGenerator(firstNum: -4,secondNum: 4),
+                                         self.randomLocationGenerator(firstNum: -4,secondNum: 4))
+        alienNode?.scale = SCNVector3(0.01, 0.01, 0.01)
         self.sceneView.scene.rootNode.addChildNode(alienNode!)
     }
 
-    @IBAction func addPlane(_ sender: Any) {        //this is the button caller for 'planez' if the button is clicked this gets called
+    //timer = NSTimer.scheduledTimerWithTimeInterval(0.3, target: self, selector: "rapidFire", userInfo: nil, repeats: true)
+    
+    @IBAction func upButtonDown(_ sender: Any) {
+        up = true
+    }
+    @IBAction func upButtonUpInside(_ sender: Any) {
+        up = false
+    }
+    @IBAction func upButtonUpOutside(_ sender: Any) {
+        up = false
+    }
+    
+    @IBAction func rightButtonDown(_ sender: Any) {
+        right = true
+    }
+    @IBAction func rightButtonUpInside(_ sender: Any) {
+        right = false
+    }
+    @IBAction func rightButtonUpOutside(_ sender: Any) {
+        right = false
+    }
+    
+    @IBAction func downButtonDown(_ sender: Any) {
+        down = true
+    }
+    @IBAction func downButtonUpInside(_ sender: Any) {
+        down = false
+    }
+    @IBAction func downButtonUpOutside(_ sender: Any) {
+        down = false
+    }
+    
+    @IBAction func leftButtonDown(_ sender: Any) {
+        left = true
+    }
+    @IBAction func leftButtonUpInside(_ sender: Any) {
+        left = false
+    }
+    @IBAction func leftButtonUpOutside(_ sender: Any) {
+        left = false
+    }
+    
+    @IBAction func planeForwardButtonDown(_ sender: Any) {
+        forward = true
+    }
+    @IBAction func planeForwardButtonUpInside(_ sender: Any) {
+        forward = false
+    }
+    @IBAction func planeForwardButtonUpOutside(_ sender: Any) {
+        forward = false
+    }
+    
+    
+    @IBAction func addAlien(_ sender: Any) {    //called if 'alienz' button is clicked
+        self.addAlienNode()
+    }
+    
+    @IBAction func addPlane(_ sender: Any) {        //called if 'planez' button is clicked
         self.addPlaneNode()
     }
     
+    func randomLocationGenerator(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    }
     
 }
 
